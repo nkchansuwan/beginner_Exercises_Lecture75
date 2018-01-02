f50e9d74d42f7cc5029fe8e2a45474b87a50dd5d
PLEKANGLE\PleKangLE <nkchansuwan@gmail.com>
2018-01-02 16:22:28 +0700

    Add project files.

---
+++ Exercises_Lecture75/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercises_Lecture75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
