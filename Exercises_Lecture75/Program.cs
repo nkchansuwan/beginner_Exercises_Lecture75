4576aefa500d7684793ae70da2a8c4d8e3029b37
PLEKANGLE\PleKangLE <nkchansuwan@gmail.com>
2018-01-04 10:57:09 +0700

    First commit

--- Exercises_Lecture75/Program.cs
+++ Exercises_Lecture75/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace Exercises_Lecture75
 {
@@ -6,7 +8,44 @@ namespace Exercises_Lecture75
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Exercise1();
+            Exercise2();
+        }
+
+        public static void Exercise1()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestFile.txt");
+
+            var allWord = File.ReadAllText(path);
+
+            var separator = new char[] { '_', '-', '/', '.', ',', ' ' };
+
+            var words = allWord.Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(words.Length);
+        }
+
+        public static void Exercise2()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "TestFile.txt");
+
+            var allWord = File.ReadAllText(path);
+
+            var separator = new char[] { '_', '-', '/', '.', ',', ' ' };
+
+            var words = allWord.Trim().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            var longestWord = "";
+
+            foreach (var item in words)
+            {
+                if (item.Length>longestWord.Length)
+                {
+                    longestWord = item;
+                }
+            }
+
+            Console.WriteLine(longestWord);
         }
     }
 }
